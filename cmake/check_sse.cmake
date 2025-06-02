890115efc2dfbc6c46bf8b33f390c1a9c2c01f77
Andrey Protsenko <58666222+andyohmyspine@users.noreply.github.com>
2025-06-02 18:13:46 +0200

    Adding support for avx

--- cmake/check_sse.cmake
+++ cmake/check_sse.cmake
@@ -11,3 +11,16 @@ elseif (MSVC)
     check_cxx_compiler_flag("/arch:AVX" AW_HAS_AVX)
     check_cxx_compiler_flag("/arch:AVX2" AW_HAS_AV2)
 endif ()
+
+cmake_host_system_information(RESULT AW_HAS_SSE QUERY HAS_SSE)
+cmake_host_system_information(RESULT AW_HAS_SSE2 QUERY HAS_SSE2)
+cmake_host_system_information(RESULT AW_HAS_SSE_FP QUERY HAS_SSE_FP)
+cmake_host_system_information(RESULT AW_HAS_SSE_MMX QUERY HAS_SSE_MMX)
+
+# Create the directory and configure the file
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/generated)
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/generated/aw)
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/generated/aw/core)
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/generated/aw/core/misc)
+configure_file(src/aw/core/aw_core_checks.h.in ${CMAKE_CURRENT_BINARY_DIR}/generated/aw/core/misc/aw_core_checks.h)
+target_include_directories(awCore PUBLIC ${CMAKE_CURRENT_BINARY_DIR}/generated)
